d5a2b9458c7ee8219b0e0e961fb005de0f8a5b9a
DrMelon <drmelon.mail@gmail.com>
2015-09-11 20:14:48 +0100

    Unity beginnings

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyPart : MonoBehaviour {
+
+    // The bodypart is used as a paperdolling aid; it will simply load an art asset from a file, sticking it onto a plane.
+    // Each part will also have offsets
+    public Material bodypartMaterial = null;
+    public Vector2 bodypartOffset;
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void InitBody(Vector2 newOffset, Material newMaterial)
+    {
+        bodypartOffset = newOffset;
+        bodypartMaterial = newMaterial;
+        ApplyOwnMaterialToBase();
+    }
+
+
+    void ApplyOwnMaterialToBase()
+    {
+        this.GetComponent<Renderer>().material = bodypartMaterial;
+    }
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/Personality.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Personality : MonoBehaviour {
+
+    // Personality Sliders, Range from -1.0 to 1.0
+    public float Slider_NiceMean = 0.0f;
+    public float Slider_OutgoingShy = 0.0f;
+    public float Slider_ActiveLazy = 0.0f;
+
+    // Visual Aspects
+    public int ChosenHead; //head includes ears and neck
+    public int ChosenEyes;
+    public int ChosenNose;
+    public int ChosenMouth;
+    public int ChosenHairFront;
+    public int ChosenHairBack;
+    public int ChosenHeadGrowths; //horns, animal ears
+     // todo- split clothing into greater categories
+    public int ChosenClothing;
+    public int ChosenJewellery;
+    // possible multiples? 
+    public int ChosenTailsWingsCapes;
+
+    // List of paperdollable parts - picked from a global list somewhere
+    public List<BodyPart> ChosenParts;
+
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    // Randomize the personality sliders
+    void RandomizePersonality()
+    {
+        
+    }
+
+    // Pick the appropriately tagged body pieces by probability markers based on personality
+    void SortAndPickPieces()
+    {
+
+    }
+
+    // Once pieces are picked, paperdoll them together to make a person
+    void PaperDolling()
+    {
+
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}

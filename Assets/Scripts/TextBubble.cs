6311e154485fdcf06a0e951ff5d88307754ab9f8
DrMelon <drmelon.mail@gmail.com>
2015-09-12 14:46:19 +0100

    First assets, text bubble

---
+++ Assets/Scripts/TextBubble.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBubble : MonoBehaviour {
+
+    // Appearing text!
+    public string TargetText;
+    public int framesBetweenLetters;
+    public Text targetGui;
+    public int currentLetter;
+    public int timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        targetGui.text = "";
+        currentLetter = 0;
+        timer = 0;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timer == 0)
+        {
+            targetGui.text = targetGui.text + TargetText.ToCharArray().GetValue(currentLetter);
+            currentLetter += 1;
+            timer = framesBetweenLetters;
+        }
+        timer--;
+        
+
+	}
+}

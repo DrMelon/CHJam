a776a9fcc93d8d20cee747a765c93f8bd18853e5
DrMelon <drmelon.mail@gmail.com>
2015-09-12 20:56:57 +0100

    More assets added. need additional tweaking

--- Assets/Scripts/HairScript.cs
+++ Assets/Scripts/HairScript.cs
@@ -16,11 +16,11 @@ public class HairScript : MonoBehaviour {
 
 
         // Depending on chosen face, try to shuffle stuff around to fit.
-        if (sprrend.sprite.name == "hair2")
+        if (sprrend.sprite.name.Contains("hair2"))
         {
             this.transform.localPosition = new Vector3(-0.25f, 2.15f, -0.5f);
         }
-        if (sprrend.sprite.name == "hair1")
+        if (sprrend.sprite.name.Contains("hair1"))
         {
             this.transform.localPosition = new Vector3(-0.46f, 2.15f, -0.5f);
         }
@@ -28,7 +28,7 @@ public class HairScript : MonoBehaviour {
         {
             this.transform.localPosition = new Vector3(-0.2f, 1.82f, -0.5f);
         }
-        if (sprrend.sprite.name == "hair3")
+        if (sprrend.sprite.name.Contains("hair4"))
         {
             this.transform.localPosition = new Vector3(-0.03f, 2.21f, -0.5f);
         }
